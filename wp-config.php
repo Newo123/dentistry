6da717096fbbcda94ed7474946f5b234a5a0c7da
Кирилл <scerep529@gmail.com>
2026-01-24 22:44:44 +0600

    feat: add gulp files and build to theme dir

--- wp-config.php
+++ wp-config.php
@@ -31,7 +31,7 @@ define( 'DB_USER', getenv('DATABASE_USER') );
 define( 'DB_PASSWORD', getenv('DATABASE_PASSWORD') );
 
 /** Database hostname */
-define( 'DB_HOST', getenv('DATABASE_DBNAME') );
+define( 'DB_HOST', getenv('DATABASE_HOST') );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
